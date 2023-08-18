49c5ca0246a18e5769aad26dabdaeec576d98b79
chenmingyong0423 <474571928@qq.com>
2023-08-19 01:43:34 +0800

    feat: 文件 doc2unix 化，go 文件代码格式化，脚本优化

--- backend/script/setup.sh
+++ backend/script/setup.sh
@@ -14,19 +14,12 @@
 
 SOURCE_COMMIT=../.github/pre-commit
 TARGET_COMMIT=../.git/hooks/pre-commit
-SOURCE_PUSH=../.github/pre-push
-TARGET_PUSH=../.git/hooks/pre-push
 
 # copy pre-commit file if not exist.
 echo "setting git pre-commit hooks..."
 cp $SOURCE_COMMIT $TARGET_COMMIT
 
-# copy pre-push file if not exist.
-echo "setting git pre-push hooks..."
-cp $SOURCE_PUSH $TARGET_PUSH
-
 # add permission to TARGET_PUSH and TARGET_COMMIT file.
-test -x $TARGET_PUSH || chmod +x $TARGET_PUSH
 test -x $TARGET_COMMIT || chmod +x $TARGET_COMMIT
 
 echo "installing golangci-lint..."
