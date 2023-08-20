a1b6d23a681efc811ed7206b42744b670491f578
fugui12321 <1183927233@qq.com>
2023-08-20 15:23:15 +0800

    feat(index):优化页面结构

--- frontend/pages/category/[name].vue
+++ frontend/pages/category/[name].vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="text-center text-30 py100 dark_text_white">
-        <div>这是文章页面</div>
-        <div>文章id:{{ route.params.id }}</div>
+    <div class="text-center text-30 py100 dark_text_white ">
+        <div>这是列表页面</div>
+        <div>名称:{{ route.params.name }}</div>
         <el-button @click="router.go(-1)">返回</el-button>
     </div>
 </template>
