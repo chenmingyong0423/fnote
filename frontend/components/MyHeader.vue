66c2e1719e8fef6059ceed66aefd684831302703
fugui12321 <1183927233@qq.com>
2023-08-24 01:07:22 +0800

    feat：新增列表页，列表项鼠标悬浮效果

--- frontend/components/MyHeader.vue
+++ frontend/components/MyHeader.vue
@@ -1,37 +1,37 @@
 <template>
-    <div class="flex justify-between items-center bg-#000/20 backdrop-blur-20 py5 dark_bg_gray ">
+    <div class="flex justify-between items-center py5 dark_bg_gray" :class="homeStore.myHeaderBg">
         <div>
             <el-space>
                 <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size="50"
                     class="mx30" />
-                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
-                    :ellipsis="false" background-color="transparent" text-color="#fff">
-                    <el-menu-item index="index">首页</el-menu-item>
-                    <el-menu-item index="list">文章列表</el-menu-item>
+                <el-menu router :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+                    :ellipsis="false" background-color="transparent" :text-color="homeStore.headerTextColor">
+                    <el-menu-item index="/">首页</el-menu-item>
+                    <el-menu-item index="/list">文章列表</el-menu-item>
                     <template v-for="item, index in menuList.data.list" :key="index">
-                        <el-menu-item :index="String(index)" v-if="!item?.tags">
-                            {{ item.name }}
-                        </el-menu-item>
-                        <el-sub-menu :index="String(index)" v-else popper-class=" bg-#000/50 dark_bg_gray important:b-0">
+                        <el-sub-menu :index="String(index)" v-if="item.tags"
+                            popper-class=" bg-#000/50 dark_bg_gray important:b-0">
                             <template #title>{{ item.name }}</template>
-                            <el-menu-item v-for="tag, index in item!.tags" :index="tag" :key="index">
+                            <el-menu-item v-for="tag, index in item.tags" :index="tag" :key="index">
                                 {{ tag }}
                             </el-menu-item>
                         </el-sub-menu>
+                        <el-menu-item :index="item.route" v-else>
+                            {{ item.name }}
+                        </el-menu-item>
                     </template>
-                    <el-menu-item index="friends">友链</el-menu-item>
-                    <el-menu-item index="about">关于我</el-menu-item>
+                    <el-menu-item index="/friends">友链</el-menu-item>
+                    <el-menu-item index="/about">关于我</el-menu-item>
                 </el-menu>
             </el-space>
         </div>
         <div>
             <el-space :size="15">
-                <el-switch size="large" v-model="value1" :active-action-icon="Moon" :inactive-action-icon="Sunny"
+                <el-switch size="large" v-model="isBlackMode" :active-action-icon="Moon" :inactive-action-icon="Sunny"
                     inactive-color="rgba(0,0,0,0.2)" active-color="#000" />
                 <div class=" bg-#1890ff rounded-50% py6 px6 dark_bg_black">
                     <div class="i-grommet-icons:search text-24 c-#fff " />
                 </div>
-
                 <div class="i-grommet-icons:github text-36 dark_text_white" />
             </el-space>
         </div>
@@ -39,9 +39,10 @@
 </template>
 
 <script lang="ts" setup>
+import { useHomeStore } from '~/store/home';
 import { Sunny, Moon } from '@element-plus/icons-vue'
-const value1 = ref<boolean>(false)
-const activeIndex = ref<string>('index')
+const isBlackMode = ref<boolean>(false)
+const activeIndex = ref<string>('/')
 const menuList = {
     "code": 200,
     "message": "OK",
@@ -58,15 +59,29 @@ const menuList = {
         ]
     }
 }
-watch(value1, () => {
-    if (value1.value == true) {
+watch(isBlackMode, (newValue) => {
+    if (newValue)
         document.querySelector('html')!.classList.add("dark");
-    }
-    else {
+    else
         document.querySelector('html')!.classList.remove("dark");
-    }
 })
 const handleSelect = (key: string, keyPath: string[]) => {
     console.log(key, keyPath)
 }
+const homeStore = useHomeStore()
+
+onMounted(() => {
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 500) {
+            homeStore.myHeaderBg = 'bg-#fff/80 backdrop-blur-60'
+            homeStore.headerTextColor = '#000'
+        }
+        else {
+            homeStore.myHeaderBg = 'bg-#000/20 backdrop-blur-20'
+            homeStore.headerTextColor = '#fff'
+        }
+    })
+})
+
+
 </script>
