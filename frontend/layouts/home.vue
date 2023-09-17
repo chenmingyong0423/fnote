6571d4ba6a2d49549897c8cb8266eb8fe1e48c97
fugui12321 <1183927233@qq.com>
2023-09-17 22:50:03 +0800

    feat:修改滚动条样式

--- frontend/layouts/home.vue
+++ frontend/layouts/home.vue
@@ -1,14 +1,30 @@
 <template>
     <div>
-        <MyHeader />
-        <div class="bg-#F0F2F5 dark_bg_black min-h-100vh">
-            <slot></slot>
+        <div ref="myDom">
+            <MyHeader />
         </div>
-        <MyFooter />
+        <el-scrollbar max-height="100vh" @scroll="headerScroll">
+            <div class="bg-#F0F2F5 dark_bg_black">
+                <slot></slot>
+            </div>
+            <MyFooter />
+        </el-scrollbar>
         <el-backtop :right="100" :bottom="100" />
     </div>
 </template>
 <script lang="ts" setup>
-
+const myDom = ref()
+let scrollCount = ref(0)
+const headerScroll = (res: any) => {
+    // const scrollNow = ref(document.documentElement.scrollTop)
+    // console.log(Math.abs(scrollNow.value - scrollCount.value));
+    if (res.scrollTop > scrollCount.value) {
+        myDom.value.firstChild.setAttribute('style', `top:-50px`);
+    }
+    else {
+        myDom.value.firstChild.setAttribute('style', 'top:0px');
+    }
+    scrollCount.value = res.scrollTop
+}
 </script>
 
