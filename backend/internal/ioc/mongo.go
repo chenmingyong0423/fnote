c6298dd8742bdc75b4511fe090dc54dfb26d8ee8
chenmingyong0423 <474571928@qq.com>
2023-09-30 00:12:54 +0800

    feat: 代码优化

--- backend/internal/ioc/mongo.go
+++ backend/internal/ioc/mongo.go
@@ -27,7 +27,7 @@ import (
 func NewMongoDB(cfg *Config) *mongo.Database {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(options.Credential{
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MongoDb.Uri).SetAuth(options.Credential{
 		Username:   cfg.MongoDb.Username,
 		Password:   cfg.MongoDb.Password,
 		AuthSource: cfg.MongoDb.AuthSource,
