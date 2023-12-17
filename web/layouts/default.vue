8e6ef46c71fc92fb8c43334cf48db3c486734aab
chenmingyong0423 <474571928@qq.com>
2023-12-17 15:24:22 +0800

    - 初始化 web 项目，nuxt3 - 修改 .gitnore 文件

---
+++ web/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div ref="myDom">
+      <Header/>
+    </div>
+    <!-- <el-scrollbar max-height="100vh" @scroll="headerScroll" /> -->
+    <div class="bg-#F0F2F5 dark_bg_black">
+      <slot></slot>
+    </div>
+  </div>
+</template>
