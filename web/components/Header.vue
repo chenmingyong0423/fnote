2c50cb5fa04f76ee58d35eb3b4e5160ba16db396
chenmingyong0423 <474571928@qq.com>
2023-12-20 01:19:05 +0800

    Header 组件优化，并加上动画效果

--- web/components/Header.vue
+++ web/components/Header.vue
@@ -1,37 +1,34 @@
 <template>
-  <div
-      class="bg-#fff  backdrop-blur-20 fixed top-0 dark:text-dtc w-full z-99 flex justify-between items-center p-1 dark_bg_gray duration-200 ease-linear">
+  <div ref="header"
+       class="bg-#fff backdrop-blur-20 fixed top-0 dark:text-dtc w-full z-99 flex justify-between items-center p-1 dark_bg_gray duration-200 ease-linear">
     <div>
-      <a-avatar :size="50" :src="picture"
-                class="mx5 cursor-pointer hover:rotate-360 ease-out duration-1000 lt-lg:mr0"></a-avatar>
+      <img :src="picture" alt=""
+           class="w-15 h-15 border-rounded-50% mx5 cursor-pointer hover:rotate-360 ease-out duration-1000 lt-lg:mr0">
     </div>
     <div class="bg_transparent">
-<!--      <a-menu v-model:selectedKeys="current"  mode="horizontal" @click="menuItemChanged" :items="items"-->
-<!--              class="dark:text-dtc bg_transparent" />-->
       <Menu :items="homeStore.menuList"></Menu>
     </div>
-    <div class="ml-auto pr-5">
-      <a-space>
-        <div class="i-ph-sun-dim-duotone dark:i-ph-moon-stars-fill cursor-pointer text-10 text-#86909c dark:text-dtc dark:hover:text-white" @click="homeStore.is_black_mode = !homeStore.is_black_mode"></div>
-        <div class="i-ph-list-magnifying-glass-duotone cursor-pointer dark:text-dtc text-10 text-#86909c dark:hover:text-white"></div>
+    <div class="ml-auto pr-5 flex gap-x-4">
+      <div
+          class="i-ph-sun-dim-duotone dark:i-ph-moon-stars-fill cursor-pointer text-10 text-#86909c dark:text-dtc dark:hover:text-white"
+          @click="homeStore.is_black_mode = !homeStore.is_black_mode"></div>
+      <div
+          class="i-ph-list-magnifying-glass-duotone cursor-pointer dark:text-dtc text-10 text-#86909c dark:hover:text-white"></div>
 
-        <div
-            class="i-grommet-icons:github text-12 dark:text-dtc cursor-pointer dark:hover:text-white active:bg-#e5e5e5 lt-lg:display-none"/>
-      </a-space>
+      <div
+          class="i-grommet-icons:github text-12 dark:text-dtc cursor-pointer dark:hover:text-white active:bg-#e5e5e5 lt-lg:display-none"/>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {h, ref, onMounted, reactive} from 'vue';
-import type {MenuProps, ItemType} from "ant-design-vue";
+import {ref, onMounted} from 'vue';
 import {getMenus} from "~/server/api/category"
 import type {IMenu} from "~/server/api/category"
 import type {IResponse, IListData} from "~/server/api/http";
 import {useHomeStore} from '~/store/home';
 import Menu from "~/components/Menu.vue";
 
-const router = useRouter()
 const homeStore = useHomeStore()
 
 const isBlackMode = computed(() => homeStore.is_black_mode)
@@ -47,16 +44,29 @@ const menus = async () => {
 };
 menus()
 
-const menuItemChanged = (item: ItemType) => {
-  router.push(item?.key as string)
-}
-
 watch(isBlackMode, (newValue) => {
   localStorage.setItem("isBlackMode", newValue.toString())
   if (newValue)
     document.querySelector('html')!.classList.add("dark");
   else
     document.querySelector('html')!.classList.remove("dark");
+})
+
+const header = ref()
+let scrollCount = ref(0)
+const headerScroll = () => {
+  if (document.documentElement.scrollTop > scrollCount.value) {
+    header.value.setAttribute('style', `top:-${header.value.clientHeight}px`);
+  } else {
+    header.value.setAttribute('style', 'top:0px');
+  }
+  scrollCount.value = document.documentElement.scrollTop
+}
+onMounted(() => {
+  window.addEventListener('scroll', headerScroll)
+})
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', headerScroll)
 })
 </script>
 
