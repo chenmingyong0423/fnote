dc80d1edf8169e96295dcceccce5987894b4f992
chenmingyong0423 <474571928@qq.com>
2023-12-23 19:13:12 +0800

    feat(分页请求体): - 修复字段命名错误的问题

--- backend/internal/pkg/api/types.go
+++ backend/internal/pkg/api/types.go
@@ -66,15 +66,15 @@ func (p *PageRequest) ValidateAndSetDefault() {
 }
 
 type Sorting struct {
-	Filed *string `form:"sortFiled,omitempty"`
+	Field *string `form:"sortField,omitempty"`
 	Order *string `form:"sortOrder,omitempty"`
 }
 
 type Page struct {
 	// 当前页
-	PageNo int64 `form:"pageNo" json:"pageNo" binding:"required"`
+	PageNo int64 `form:"pageNo" binding:"required"`
 	// 每页数量
-	PageSize int64 `form:"pageSize" json:"pageSize" binding:"required"`
+	PageSize int64 `form:"pageSize" binding:"required"`
 }
 
 type ListVO[T any] struct {
