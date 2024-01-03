11b0c85df8995e0cabafb9cf2e0c3c32316bdd80
chenmingyong0423 <474571928@qq.com>
2024-01-03 20:18:52 +0800

    补充 file 目录

---
+++ server/internal/file/handler/file_handler.go
@@ -0,0 +1,76 @@
+// Copyright 2024 chenmingyong0423
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package handler
+
+import (
+	"io"
+	"path/filepath"
+
+	"github.com/chenmingyong0423/fnote/server/internal/pkg/web/vo"
+
+	"github.com/chenmingyong0423/fnote/server/internal/pkg/web/dto"
+
+	"github.com/chenmingyong0423/fnote/server/internal/file/service"
+	"github.com/chenmingyong0423/fnote/server/internal/pkg/api"
+	"github.com/gin-gonic/gin"
+)
+
+func NewFileHandler(serv service.IFileService) *FileHandler {
+	return &FileHandler{
+		serv: serv,
+	}
+}
+
+type FileHandler struct {
+	serv service.IFileService
+}
+
+func (h *FileHandler) RegisterGinRoutes(engine *gin.Engine) {
+	adminGroup := engine.Group("/admin/files")
+	adminGroup.POST("/upload", api.Wrap(h.UploadFile))
+}
+
+func (h *FileHandler) UploadFile(ctx *gin.Context) (VO vo.FileVO, err error) {
+	file, err := ctx.FormFile("file")
+	if err != nil {
+		return
+	}
+	openedFile, err := file.Open()
+	if err != nil {
+		return
+	}
+	defer openedFile.Close()
+	content, err := io.ReadAll(openedFile)
+	if err != nil {
+		return
+	}
+	fileDto := dto.FileDTO{
+		FileName: file.Filename,
+		FileSize: file.Size,
+		Content:  content,
+		FileType: file.Header.Get("Content-Type"),
+		FileExt:  filepath.Ext(file.Filename),
+	}
+	fileInfo, err := h.serv.Upload(ctx, fileDto)
+	if err != nil {
+		return
+
+	}
+	return vo.FileVO{
+		FileId:   fileInfo.FileId,
+		FileName: fileInfo.FileName,
+		Url:      fileInfo.Url,
+	}, nil
+}
---
+++ server/internal/file/repository/dao/file_dao.go
@@ -0,0 +1,70 @@
+// Copyright 2024 chenmingyong0423
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dao
+
+import (
+	"context"
+
+	"github.com/chenmingyong0423/go-mongox"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type File struct {
+	Id               primitive.ObjectID `bson:"_id,omitempty"`
+	FileId           []byte             `bson:"file_id"`
+	FileName         string             `bson:"file_name"`
+	OriginalFileName string             `bson:"original_file_name"`
+	FileType         string             `bson:"file_type"`
+	FileSize         int64              `bson:"size"`
+	FilePath         string             `bson:"file_path"`
+	Url              string             `bson:"url"`
+	UsedIn           []FileUsage        `bson:"used_in"`
+	CreateTime       int64              `bson:"create_time"`
+	UpdateTime       int64              `bson:"update_time"`
+}
+
+type FileUsage struct {
+	EntityId   string     `bson:"entity_id"`
+	EntityType EntityType `bson:"entity_type"`
+}
+
+type EntityType string
+
+const (
+	EntityTypePost EntityType = "post"
+)
+
+type IFileDao interface {
+	Save(ctx context.Context, file *File) (string, error)
+}
+
+var _ IFileDao = (*FileDao)(nil)
+
+func NewFileDao(db *mongo.Database) *FileDao {
+	return &FileDao{coll: mongox.NewCollection[File](db.Collection("file_meta"))}
+}
+
+type FileDao struct {
+	coll *mongox.Collection[File]
+}
+
+func (d *FileDao) Save(ctx context.Context, file *File) (string, error) {
+	oneResult, err := d.coll.Creator().InsertOne(ctx, *file)
+	if err != nil {
+		return "", err
+	}
+	return oneResult.InsertedID.(primitive.ObjectID).Hex(), nil
+}
