f66f9c4061c133e532aecd83576cd519e100b18e
chenmingyong0423 <474571928@qq.com>
2024-01-28 14:15:09 +0800

    - SmallMenu 组件修复动态菜单不显示的问题

--- web/components/SmallMenu.vue
+++ web/components/SmallMenu.vue
@@ -2,25 +2,26 @@
   <div>
     <div
       class="duration-400 fixed z-999 backdrop-blur-md bg-white bg-opacity-40 md:hidden flex items-center justify-end custom_shadow"
-      :class="info.showSmallScreenMenu ? 'inset-0' : 'inset-full'"
-      @click="info.showSmallScreenMenu = false"
+      :class="homeStore.showSmallScreenMenu ? 'inset-0' : 'inset-full'"
+      @click="homeStore.showSmallScreenMenu = false"
     ></div>
 
     <div
       class="fixed z-999 right-0 bg-white w-70% h-full bg-#f7f9fe custom_border_gray duration-300 md:hidden dark:text-dtc dark_bg_full_black"
-      :class="info.showSmallScreenMenu ? ' translate-x-0' : ' translate-x-100%'"
+      :class="homeStore.showSmallScreenMenu ? ' translate-x-0' : ' translate-x-100%'"
     >
       <Profile></Profile>
       <Menu
-        :items="homeStore.menuList"
-        @click="info.showSmallScreenMenu = false"
+        :items="menuStore.menuList"
+        @click="homeStore.showSmallScreenMenu = false"
       ></Menu>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { useHomeStore } from "~/store/home";
+import {useMenuStore} from "~/store/menu";
 
-const info = useHomeStore();
 const homeStore = useHomeStore();
+const menuStore = useMenuStore();
 </script>
