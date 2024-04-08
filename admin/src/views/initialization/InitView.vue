66110a20c88442ebc0fd695a09bccab4a38c4ccc
Mingyong Chen <chenmingyong5873@126.com>
2024-04-08 22:20:00 +0800

    恢复暂存

--- admin/src/views/initialization/InitView.vue
+++ admin/src/views/initialization/InitView.vue
@@ -2,7 +2,10 @@
   <div class="flex items-center justify-center h-screen bg-light-blue">
     <div class="w-96 p-8 bg-white rounded-lg shadow-md modal">
       <div class="flex justify-center mb-6">
-        <img src="@/assets/logo.png" alt="Logo" class="w-60 h-30" />
+        <img src="@/assets/logo.png" alt="Logo" class="w-80 h-40" />
+      </div>
+      <div class="flex justify-center mb-6">
+        {{ stepInfo[step - 1] }}
       </div>
       <a-form
         ref="formRef"
@@ -11,13 +14,13 @@
         class="login-form"
         @finish="submit"
         @finishFailed="onFinishFailed"
-        labelAlign="left"
+        :labelCol="{span: labelCols[step-1]}"
       >
         <div v-show="step == 1">
           <a-form-item
             label="站点名称"
             name="website_name"
-            :rules="[{ required: true, message: '请输入站点昵称！' }]"
+            :rules="[{ required: true, message: '请输入站点昵称' }]"
           >
             <a-input v-model:value="initReq.website_name">
             </a-input>
@@ -26,7 +29,7 @@
           <a-form-item
             label="站长昵称"
             name="website_owner"
-            :rules="[{ required: true, message: '请输入站长昵称！' }]"
+            :rules="[{ required: true, message: '请输入站长昵称' }]"
           >
             <a-input v-model:value="initReq.website_owner">
             </a-input>
@@ -34,34 +37,27 @@
           <a-form-item
             label="域名"
             name="website_domain"
-            :rules="[{ required: true, message: '请输入域名！' }]"
+            :rules="[{ required: true, message: '请输入域名' }]"
           >
             <a-input v-model:value="initReq.website_domain">
             </a-input>
           </a-form-item>
           <a-form-item
+            label="邮箱地址"
+            name="website_owner_email"
+            :rules="[{ required: true, message: '请输入邮箱地址' }]"
+            tooltip="接收通知时使用"
+          >
+            <a-input v-model:value="initReq.website_owner_email">
+            </a-input>
+          </a-form-item>
+          <a-form-item
             label="站长简介"
             name="website_owner_profile"
-            :rules="[{ required: true, message: '请输入站长简介！' }]"
+            :rules="[{ required: true, message: '请输入站长简介' }]"
           >
             <a-textarea v-model:value="initReq.website_owner_profile" :auto-size="{ minRows: 2, maxRows: 3}">
             </a-textarea>
-          </a-form-item>
-
-          <a-form-item
-            label="站点Logo"
-            name="website_icon"
-            :rules="[{ required: true, message: '请输入上传站点 logo！' }]"
-          >
-            <AvatarUpload :image-url="initReq.website_icon" @update:imageUrl="value => handleWebsiteIcon(value)"/>
-          </a-form-item>
-
-          <a-form-item
-            label="站长头像"
-            name="website_owner_avatar"
-            :rules="[{ required: true, message: '请输入上传站长头像！' }]"
-          >
-            <AvatarUpload :imageUrl="initReq.website_owner_avatar" @update:imageUrl="value => handleOwnerAvatar(value)"/>
           </a-form-item>
 
           <a-form-item>
@@ -76,23 +72,21 @@
         </div>
         <div v-show="step == 2">
           <a-form-item
-            label="seo - 站点标题"
-            :name="['seo', 'title']"
-            :rules="[{ required: true, message: '请输入 seo - 站点标题！' }]"
+            label="站点 Logo"
+            name="website_icon"
+            :rules="[{ required: true, message: '请输入上传站点 logo' }]"
           >
-            <a-textarea v-model:value="initReq.seo.title">
-            </a-textarea>
+            <StaticUpload :image-url="initReq.website_icon" @update:imageUrl="value => handleWebsiteIcon(value)" />
           </a-form-item>
 
           <a-form-item
-            label="seo- 站点描述"
-            :name="['seo', 'description']"
-            :rules="[{ required: true, message: '请输入 seo- 站点描述！' }]"
+            label="站长头像"
+            name="website_owner_avatar"
+            :rules="[{ required: true, message: '请输入上传站长头像' }]"
           >
-            <a-input v-model:value="initReq.seo.description">
-            </a-input>
+            <StaticUpload :image-url="initReq.website_owner_avatar"
+                          @update:imageUrl="value => handleOwnerAvatar(value)" />
           </a-form-item>
-
 
           <a-form-item>
             <a-button
@@ -111,40 +105,65 @@
             </a-button>
           </a-form-item>
         </div>
-<!--        <a-form-item-->
-<!--          label=""-->
-<!--          name="username"-->
-<!--          :rules="[{ required: true, message: '请输入用户名！' }]"-->
-<!--        >-->
-<!--          <a-input v-model:value="loginReq.username">-->
-<!--            <template #prefix>-->
-<!--              <UserOutlined class="site-form-item-icon" />-->
-<!--            </template>-->
-<!--          </a-input>-->
-<!--        </a-form-item>-->
 
-<!--        <a-form-item-->
-<!--          label=""-->
-<!--          name="password"-->
-<!--          :rules="[{ required: true, message: '请输入密码！' }]"-->
-<!--        >-->
-<!--          <a-input-password v-model:value="loginReq.password">-->
-<!--            <template #prefix>-->
-<!--              <LockOutlined class="site-form-item-icon" />-->
-<!--            </template>-->
-<!--          </a-input-password>-->
-<!--        </a-form-item>-->
+        <div v-show="step == 3">
+          <a-form-item
+            label="host"
+            :name="['email_server', 'host']"
+            :rules="[{ required: true, message: '请输入邮件服务的 host' }]"
+          >
+            <a-input v-model:value="initReq.email_server.host" />
+          </a-form-item>
 
-<!--        <a-form-item>-->
-<!--          <a-button-->
-<!--            :disabled="disabled"-->
-<!--            type="primary"-->
-<!--            html-type="submit"-->
-<!--            class="login-form-button w-full h-10"-->
-<!--          >-->
-<!--            登录-->
-<!--          </a-button>-->
-<!--        </a-form-item>-->
+          <a-form-item
+            label="port"
+            :name="['email_server', 'port']"
+            :rules="[{ required: true, message: '请输入邮件服务的 port' }]"
+          >
+            <a-input v-model:value="initReq.email_server.port" />
+          </a-form-item>
+
+          <a-form-item
+            label="username"
+            :name="['email_server', 'username']"
+            :rules="[{ required: true, message: '请输入邮件服务的 username' }]"
+          >
+            <a-input v-model:value="initReq.email_server.username" />
+          </a-form-item>
+
+          <a-form-item
+            label="password"
+            :name="['email_server', 'password']"
+            :rules="[{ required: true, message: '请输入邮件服务的 password' }]"
+          >
+            <a-input v-model:value="initReq.email_server.password" />
+          </a-form-item>
+
+          <a-form-item
+            label="email"
+            :name="['email_server', 'email']"
+            :rules="[{ required: true, message: '请输入邮件服务的 email' }]"
+          >
+            <a-input v-model:value="initReq.email_server.email" />
+          </a-form-item>
+
+          <a-form-item>
+            <a-button
+              type="primary"
+              class="login-form-button w-40% h-10"
+              @click="step--"
+            >
+              上一步
+            </a-button>
+            <a-button
+              type="primary"
+              class="login-form-button w-40% h-10 float-right"
+              @click="step++"
+            >
+              下一步
+            </a-button>
+          </a-form-item>
+        </div>
       </a-form>
     </div>
   </div>
@@ -157,30 +176,31 @@ import { message } from 'ant-design-vue'
 import type { InitReq } from '@/interfaces/Config'
 import router from '@/router'
 import { useUserStore } from '@/stores/user'
-import AvatarUpload from '@/components/upload/AvatarUpload.vue'
+import StaticUpload from '@/components/upload/StaticUpload.vue'
 
 const userStore = useUserStore()
 
 const initReq = reactive<InitReq>({
-  website_name: "",
-  website_icon: "",
-  website_owner: "",
-  website_owner_profile: "",
-  website_owner_avatar: "",
-  website_domain: "",
-  seo: {
-    title: "",
-    description: "",
-    og_title: "",
-    og_image: "",
-    baidu_site_verification: "",
-    keywords: "",
-    author: "",
-    robots: ""
+  website_name: '',
+  website_icon: '',
+  website_owner: '',
+  website_owner_profile: '',
+  website_owner_avatar: '',
+  website_domain: '',
+  website_owner_email: '',
+  email_server: {
+    host: '',
+    port: '',
+    username: '',
+    password: '',
+    email: '',
   }
 })
 
 const step = ref(1)
+
+const stepInfo = ['站点信息', '站点信息', '邮件服务器信息']
+const labelCols = [7, 7, 7]
 
 const onFinishFailed = (errorInfo: any) => {
   console.log('Failed:', errorInfo)
@@ -191,17 +211,17 @@ const submit = async () => {
   message.success('初始化成功')
 }
 
-const formRef = ref();
-const handleWebsiteIcon = (value:string) => {
-  initReq.website_icon = value;
+const formRef = ref()
+const handleWebsiteIcon = (value: string) => {
+  initReq.website_icon = value
   // 现在手动触发验证
-  formRef.value?.validateFields(['website_icon']);
-};
-const handleOwnerAvatar = (value:string) => {
-  initReq.website_owner_avatar = value;
+  formRef.value?.validateFields(['website_icon'])
+}
+const handleOwnerAvatar = (value: string) => {
+  initReq.website_owner_avatar = value
   // 现在手动触发验证
-  formRef.value?.validateFields(['website_owner_avatar']);
-};
+  formRef.value?.validateFields(['website_owner_avatar'])
+}
 
 
 </script>
