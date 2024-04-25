f7eb97425415ab6891cde07ac61f7d74ff530d00
陈明勇 <chenmingyong5873@126.com>
2024-04-25 11:59:29 +0800

    完成设置 title,meta 属性的功能

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import HomeView from '@/views/HomeView.vue'
+import { useWebsiteStore } from '@/stores/website'
+import { GetWebSiteMeta, type WebsiteConfigMetaVO } from '@/interfaces/Config'
+
+const websiteStore = useWebsiteStore()
+const  initWebsiteStore = async () => {
+  if (websiteStore.website_name === '' && websiteStore.website_icon === '') {
+    try {
+      const response: any = await GetWebSiteMeta()
+      if (response.data.code === 0) {
+        const data : WebsiteConfigMetaVO = response.data.data
+        websiteStore.Update(data.website_name || 'fnote', data.website_icon)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+initWebsiteStore()
+
 </script>
 
 <template>
-  <!--  <HomeView></HomeView>-->
   <RouterView />
 </template>
 
