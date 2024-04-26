a2a5a4bdcd429bdb552918a1168cc8ef91a4e755
Mingyong Chen <chenmingyong5873@126.com>
2024-04-27 01:28:51 +0800

    后台： - highlight.js 优化

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import router from '@/router'
 
 import VMdEditor from '@kangc/v-md-editor'
 import '@kangc/v-md-editor/lib/style/base-editor.css'
@@ -12,9 +13,22 @@ import '@kangc/v-md-editor/lib/theme/style/github.css'
 import 'uno.css'
 import 'virtual:uno.css'
 
-// highlightjs
-import hljs from 'highlight.js'
-import router from '@/router'
+// highlightjs 核心代码
+import hljs from 'highlight.js/lib/core';
+// 按需引入语言包
+import c from 'highlight.js/lib/languages/c';
+import shell from 'highlight.js/lib/languages/shell';
+import go from 'highlight.js/lib/languages/go';
+import java from 'highlight.js/lib/languages/java';
+import python from 'highlight.js/lib/languages/python';
+import json from 'highlight.js/lib/languages/json';
+
+hljs.registerLanguage('c', c);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('go', go);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('json', json);
 
 VMdEditor.use(githubTheme, {
   Hljs: hljs
