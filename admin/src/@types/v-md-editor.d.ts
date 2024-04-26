6967ac8e901546147034b0295c0caef0cc7a078a
Mingyong Chen <chenmingyong5873@126.com>
2024-04-27 01:28:51 +0800

    后台： - highlight.js 优化

--- admin/src/@types/v-md-editor.d.ts
+++ admin/src/@types/v-md-editor.d.ts
@@ -1,2 +1,11 @@
 declare module '@kangc/v-md-editor'
 declare module '@kangc/v-md-editor/lib/theme/github.js'
+
+declare module 'highlight.js/lib/core';
+// 按需引入语言包
+declare module 'highlight.js/lib/languages/c';
+declare module 'highlight.js/lib/languages/shell';
+declare module 'highlight.js/lib/languages/go';
+declare module 'highlight.js/lib/languages/java';
+declare module 'highlight.js/lib/languages/python';
+declare module 'highlight.js/lib/languages/json';
