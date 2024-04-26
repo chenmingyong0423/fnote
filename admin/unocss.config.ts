f349383c9640dbfd34817b3226ff3e2113cc66c1
Mingyong Chen <chenmingyong5873@126.com>
2024-04-27 02:05:32 +0800

    后台： - 解决 unocss icons 不显示的问题 - 代码格式化

--- admin/unocss.config.ts
+++ admin/unocss.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig, presetUno, presetIcons } from 'unocss'
-import icons from '@iconify-json/bi'
 export default defineConfig({
   // 这里可以添加预设、规则等配置
   presets: [
     // 使用预设
     presetUno(),
-    presetIcons({
-      // 其他选项
-      prefix: 'i-',
-      extraProperties: {
-        display: 'inline-block'
-      }
-    }),
-    icons
+    presetIcons()
   ],
   rules: [
     // 自定义规则
