febab5dd4a6ecb5f9b102c661822f5e3cea09409
Mingyong Chen <chenmingyong5873@126.com>
2024-04-26 23:14:06 +0800

    后台：去掉无用的图标库

--- admin/unocss.config.ts
+++ admin/unocss.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig, presetUno, presetIcons } from 'unocss'
-import icons from '@iconify-json/bi'
 export default defineConfig({
   // 这里可以添加预设、规则等配置
   presets: [
     // 使用预设
     presetUno(),
-    presetIcons(),
-    icons
+    presetIcons()
   ],
   rules: [
     // 自定义规则
