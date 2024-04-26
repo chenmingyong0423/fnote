8cf1bc22e0a360fda94646e74207099e0539afce
Mingyong Chen <chenmingyong5873@126.com>
2024-04-26 22:07:07 +0800

    后台： - 完成流量统计页面的接口对接

--- admin/src/interfaces/DataAnalysis.ts
+++ admin/src/interfaces/DataAnalysis.ts
@@ -1,19 +1,29 @@
 import instance from '@/utils/axios'
 
-export interface DataAnalysisVO {
-  post_count: number
-  category_count: number
-  tag_count: number
+
+export interface TodayTrafficStatsVO {
+  view_count: number
+  user_view_count: number
   comment_count: number
   like_count: number
-  today_view_count: number
-  total_view_count: number
-  today_user_visit_count: number
 }
 
-export const GetDataAnalysis = () => {
+export interface TrafficStatsVO {
+  view_count: number
+  comment_count: number
+  like_count: number
+}
+
+export const GetTodayTrafficStats = () => {
   return instance({
-    url: '/data-analysis',
+    url: '/data-analysis/traffic/today',
+    method: 'get'
+  })
+}
+
+export const GetTrafficStats = () => {
+  return instance({
+    url: '/data-analysis/traffic',
     method: 'get'
   })
 }
