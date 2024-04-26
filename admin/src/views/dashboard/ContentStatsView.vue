9ae093743856b07f6c09170871eb690f1f7af868
Mingyong Chen <chenmingyong5873@126.com>
2024-04-26 22:15:31 +0800

    后台： - 完成内容发布统计页面的接口对接

--- admin/src/views/dashboard/ContentStatsView.vue
+++ admin/src/views/dashboard/ContentStatsView.vue
@@ -1,14 +1,34 @@
 <template>
   <a-card title="总览">
     <a-flex gap="middle" horizontal>
-      <a-statistic title="文章数量" :value="0" class="w-33%" />
-      <a-statistic title="分类数量" :value="0" class="w-33%" />
-      <a-statistic title="标签数量" :value="0" class="w-33%" />
+      <a-statistic title="文章数量" :value="contentStatsVO.post_count" class="w-33%" />
+      <a-statistic title="分类数量" :value="contentStatsVO.category_count" class="w-33%" />
+      <a-statistic title="标签数量" :value="contentStatsVO.tag_count" class="w-33%" />
     </a-flex>
   </a-card>
 </template>
 
 <script lang="ts" setup>
+import { type ContentStatsVO, GetContentStatsVO } from '@/interfaces/DataAnalysis'
+import { message } from 'ant-design-vue'
+import { ref } from 'vue'
 
+
+const contentStatsVO = ref<ContentStatsVO>({
+
+})
+const getContentStatsVO = async () => {
+  try {
+    const response: any = await GetContentStatsVO()
+    if (response.data.code !== 0) {
+      message.error(response.data.data.message)
+      return
+    }
+    contentStatsVO.value = response.data.data || contentStatsVO.value
+  } catch (error) {
+    console.log(error)
+  }
+}
+getContentStatsVO()
 
 </script>
