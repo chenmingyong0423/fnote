8d5b273f3d3ff429e54b8e8323c4eaa5d017b841
陈明勇 <chenmingyong5873@126.com>
2025-06-23 15:11:33 +0800

    重构 footer 组件

--- nextjs-web/src/components/Footer.tsx
+++ nextjs-web/src/components/Footer.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 const Footer: React.FC = () => (
-  <footer className="w-full bg-white text-center py-4 text-gray-500 text-sm border-t border-gray-200 mt-8 h-[60px]">
-    © {new Date().getFullYear()} Copyright © 2024 - Designed by <a href="https://github.com/chenmingyong0423/fnote">Fnote</a>
+  <footer className="w-full bg-white border-t border-gray-200 mt-8 h-[60px] px-4 mx-auto max-w-7xl grid grid-cols-12 items-center">
+    {/* 左留白 */}
+    <div className="col-span-1 lg:col-span-2" />
+    {/* 主体内容 10/12 或 8/12 居中 */}
+    <div className="col-span-10 lg:col-span-8 flex items-center justify-center w-full h-full text-center text-gray-500 text-sm">
+      © {new Date().getFullYear()} Copyright © 2024 - Designed by{" "}
+      <a
+        href="https://github.com/chenmingyong0423/fnote"
+        className="ml-1 text-blue-600 hover:underline"
+      >
+        Fnote
+      </a>
+    </div>
+    {/* 右留白 */}
+    <div className="col-span-1 lg:col-span-2" />
   </footer>
 );
 
