5328d818966ef3837052569fbc295726fc8c4576
陈明勇 <chenmingyong5873@126.com>
2025-06-30 16:13:48 +0800

    解决 antd 图片组件警告的问题

--- nextjs-web/src/components/FeaturedCarousel.tsx
+++ nextjs-web/src/components/FeaturedCarousel.tsx
@@ -15,10 +15,10 @@ export default function FeaturedCarousel({ items }: { items: CarouselItemVO[] })
             className="block relative h-56 sm:h-72 focus:outline-none"
             tabIndex={0}
           >
-            <div className="w-full h-full flex items-center justify-center bg-gray-100">
+            <div className="w-full h-full flex items-center justify-center bg-gray-100 relative">
               {/* 图片展示 */}
               {item.cover_img && (
-                <Image src={item.cover_img} alt={item.title} fill sizes="(max-width: 768px) 100vw, 100vw" className="object-fill rounded-lg" />
+                <Image src={item.cover_img} alt={item.title} fill sizes="60" className="rounded-lg" priority />
               )}
               {/* 文字遮罩 */}
               <div className="absolute bottom-0 left-0 right-0 bg-black/50 text-white p-4 rounded-b-lg">
