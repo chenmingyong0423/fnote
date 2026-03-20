42a99c8730548469a681a0961f922c1099d53b6b
chenmingyong0423 <474571928@qq.com>
2026-03-20 18:35:09 +0800

    admin: 配置文件优化

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -3,30 +3,45 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import { visualizer } from 'rollup-plugin-visualizer';
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from 'rollup-plugin-visualizer'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    UnoCSS(),
-    visualizer({ open: true, gzipSize: true, brotliSize: true }),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false, // css in js
+export default defineConfig(({ mode }) => {
+  const isDocker = mode === 'docker'
+
+  return {
+    plugins: [
+      vue(),
+      UnoCSS(),
+
+      // 🚨 关键：Docker 时禁用 visualizer
+      !isDocker &&
+        visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true
         }),
-      ],
-    }),
-  ],
-  base: `/${process.env.VITE_BUILD_DIR || ""}`,
-  build: {
-    outDir: `dist/${process.env.VITE_BUILD_DIR || ""}`,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false
+          })
+        ]
+      })
+    ].filter(Boolean),
+
+    base: `/${process.env.VITE_BUILD_DIR || ''}`,
+
+    build: {
+      outDir: `dist/${process.env.VITE_BUILD_DIR || ''}`
+    },
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
