9abbdad3539f0cd634e20e4890999ade3738a9a9
chenmingyong0423 <474571928@qq.com>
2026-06-24 10:05:20 +0800

    fix: create unique Mongo replica keyfile on startup

--- docker-compose/script/mongo-replica-entrypoint.sh
+++ docker-compose/script/mongo-replica-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-keyfile=/tmp/mongo-replica-keyfile
+keyfile=$(mktemp /tmp/mongo-replica-keyfile.XXXXXX)
 printf '%s' "${MONGO_REPLICA_SET_KEY:-fnote-local-replica-set-key-change-me}" | base64 | tr -d '\n' > "${keyfile}"
 chmod 400 "${keyfile}"
-chown mongodb:mongodb "${keyfile}"
+if [ "$(id -u)" = "0" ]; then
+  chown mongodb:mongodb "${keyfile}"
+fi
 
 exec docker-entrypoint.sh "$@" --replSet "${MONGO_REPLICA_SET_NAME:-rs0}" --keyFile "${keyfile}" --bind_ip_all
